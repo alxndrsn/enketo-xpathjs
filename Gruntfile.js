e76d198f450bb8608fc44959d1077bc6d15f2f55
Martijn van de Rijdt <martijn@enketo.org>
2015-03-04 17:44:21 -0700

    started converting tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,10 +9,22 @@ module.exports = function(grunt) {
 					port: 8080
 				}
 			}
+		},
+		karma: {
+			options: {
+				singleRun: true,
+				reporters: ['dots']
+			},
+			headless: {
+				configFile: 'test/karma.conf.js',
+				browsers: ['PhantomJS']
+			}
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-php');
+	grunt.loadNpmTasks('grunt-karma');
+
 	grunt.registerTask('server', ['php:server:keepalive']);
 	grunt.registerTask('default', ['php:server:keepalive']);
 };
