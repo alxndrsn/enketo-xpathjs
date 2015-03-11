928488bcdc71be6518808ac5c9cfe9d239e714ef
Martijn van de Rijdt <martijn@enketo.org>
2015-03-11 09:48:12 -0600

    cleaned up namespace resolver tests

--- test/spec/namespace-resolver-native.spec.js
+++ test/spec/namespace-resolver-native.spec.js
@@ -9,21 +9,15 @@ describe('namespace resolver', function() {
 
         // check type
         expect(resolver).to.be.an.instanceOf(win.XPathNSResolver);
-        //Y.Assert.isInstanceOf(win.XPathNSResolver, resolver);
         expect(resolver.lookupNamespaceURI).to.be.a('function');
-        //Y.Assert.areSame("function", typeof resolver.lookupNamespaceURI); 
 
         // check preconfigured namespaces
         expect(resolver.lookupNamespaceURI('xml')).to.equal('http://www.w3.org/XML/1998/namespace');
-        //Y.Assert.areSame('http://www.w3.org/XML/1998/namespace', resolver.lookupNamespaceURI('xml'));
         expect(resolver.lookupNamespaceURI('xmlns')).to.equal('http://www.w3.org/2000/xmlns/');
-        //Y.Assert.areSame('http://www.w3.org/2000/xmlns/', resolver.lookupNamespaceURI('xmlns')); 
 
         // check namespaces on current element
         expect(resolver.lookupNamespaceURI('xforms')).to.equal('http://www.w3.org/2002/xforms');
-        //Y.Assert.areSame('http://www.w3.org/2002/xforms', resolver.lookupNamespaceURI('xforms'));
         expect(resolver.lookupNamespaceURI('nsnotexists')).to.equal(null);
-        //Y.Assert.areSame(null, resolver.lookupNamespaceURI('nsnotexists'));
 
         // check default namespace
         resolver = documentCreateNSResolver(helpers.getNextChildElementNode(node));
@@ -35,13 +29,10 @@ describe('namespace resolver', function() {
         var resolver = documentCreateNSResolver(doc);
 
         expect(resolver).to.be.an.instanceOf(win.XPathNSResolver);
-        //Y.Assert.isInstanceOf(win.XPathNSResolver, resolver);
 
         expect(resolver.lookupNamespaceURI).to.be.a('function');
-        //Y.Assert.areSame("function", typeof resolver.lookupNamespaceURI);
 
         expect(resolver.lookupNamespaceURI('ev')).to.equal('http://some-namespace.com/nss');
-        //Y.Assert.areSame('http://some-namespace.com/nss', resolver.lookupNamespaceURI('ev'));
     });
 
     it('looks up the namespaceURIDocumentElement', function() {
@@ -49,29 +40,22 @@ describe('namespace resolver', function() {
         var resolver = documentCreateNSResolver(node);
 
         expect(resolver).to.be.an.instanceOf(win.XPathNSResolver);
-        //Y.Assert.isInstanceOf(win.XPathNSResolver, resolver);
         expect(resolver.lookupNamespaceURI).to.be.a('function');
-        //Y.Assert.areSame("function", typeof resolver.lookupNamespaceURI);
 
         expect(resolver.lookupNamespaceURI('ev')).to.equal('http://some-namespace.com/nss');
-        //Y.Assert.areSame('http://some-namespace.com/nss', resolver.lookupNamespaceURI('ev'));
         expect(resolver.lookupNamespaceURI('')).to.equal('http://www.w3.org/1999/xhtml');
-        //Y.Assert.areSame('http://www.w3.org/1999/xhtml', resolver.lookupNamespaceURI(''));
 
         // Make sure default xhtml namespace is correct
         node.removeAttribute('xmlns');
         expect(resolver.lookupNamespaceURI('')).to.equal(null);
-        //Y.Assert.areSame(null, resolver.lookupNamespaceURI(''));
 
         // Change default root namespace
         helpers.setAttribute(node, 'http://www.w3.org/2000/xmlns/', 'xmlns', 'some-namespace');
         expect(resolver.lookupNamespaceURI('')).to.equal('some-namespace');
-        //Y.Assert.areSame('some-namespace', resolver.lookupNamespaceURI(''));
 
         // Revert back to default xhtml namespace
         helpers.setAttribute(node, 'http://www.w3.org/2000/xmlns/', 'xmlns', 'http://www.w3.org/1999/xhtml');
         expect(resolver.lookupNamespaceURI('')).to.equal('http://www.w3.org/1999/xhtml');
-        //Y.Assert.areSame('http://www.w3.org/1999/xhtml', resolver.lookupNamespaceURI(''));
     });
 
     it('looks up the namespaceURIAttribute', function() {
@@ -87,11 +71,9 @@ describe('namespace resolver', function() {
         }
 
         expect(attribute).to.be.an('object');
-        //Y.Assert.isObject(attribute);
 
         resolver = documentCreateNSResolver(attribute);
         expect(resolver.lookupNamespaceURI('ev')).to.equal('http://some-namespace.com/nss');
-        //Y.Assert.areSame('http://some-namespace.com/nss', resolver.lookupNamespaceURI('ev')); //
 
         // Check parent nodes for default namespace declaration
         attribute = null;
@@ -105,11 +87,9 @@ describe('namespace resolver', function() {
         }
 
         expect(attribute).to.be.an('object');
-        //Y.Assert.isObject(attribute);
 
         resolver = documentCreateNSResolver(attribute);
         expect(resolver.lookupNamespaceURI('xforms')).to.equal('http://www.w3.org/2002/xforms');
-        //Y.Assert.areSame('http://www.w3.org/2002/xforms', resolver.lookupNamespaceURI('xforms'));
     });
 
     it('looks up namespaceURIs that have changed', function() {
@@ -117,27 +97,22 @@ describe('namespace resolver', function() {
         var resolver = documentCreateNSResolver(node);
 
         expect(resolver.lookupNamespaceURI('')).to.equal('http://www.w3.org/TR/REC-html40');
-        //Y.Assert.areSame('http://www.w3.org/TR/REC-html40', resolver.lookupNamespaceURI(''));
 
         // Remove default namespace
         node.removeAttribute('xmlns');
         expect(resolver.lookupNamespaceURI('')).to.equal('http://www.w3.org/1999/xhtml');
-        //Y.Assert.areSame('http://www.w3.org/1999/xhtml', resolver.lookupNamespaceURI(''));
 
         // Change default namespace to some other namespace
         helpers.setAttribute(node, 'http://www.w3.org/2000/xmlns/', 'xmlns', 'some-namespace');
         expect(resolver.lookupNamespaceURI('')).to.equal('some-namespace');
-        //Y.Assert.areSame('some-namespace', resolver.lookupNamespaceURI(''));
 
         // No default namespace
         helpers.setAttribute(node, 'http://www.w3.org/2000/xmlns/', 'xmlns', '');
         expect(resolver.lookupNamespaceURI('')).to.equal('');
-        //Y.Assert.areSame('', resolver.lookupNamespaceURI(''));
 
         // Back to original
         helpers.setAttribute(node, 'http://www.w3.org/2000/xmlns/', 'xmlns', 'http://www.w3.org/TR/REC-html40');
         expect(resolver.lookupNamespaceURI('')).to.equal('http://www.w3.org/TR/REC-html40');
-        //Y.Assert.areSame('http://www.w3.org/TR/REC-html40', resolver.lookupNamespaceURI(''));
     });
 
     it('looks up a hierarchical namespaceURI', function() {
@@ -146,16 +121,13 @@ describe('namespace resolver', function() {
 
         // check prefix in parents
         expect(resolver.lookupNamespaceURI('ev')).to.equal('http://some-namespace.com/nss');
-        //Y.Assert.areSame('http://some-namespace.com/nss', resolver.lookupNamespaceURI('ev'));
 
         // check default prefix in parents
         expect(resolver.lookupNamespaceURI('')).to.equal('http://www.w3.org/1999/xhtml');
-        //Y.Assert.areSame('http://www.w3.org/1999/xhtml', resolver.lookupNamespaceURI(''));
 
         resolver = documentCreateNSResolver(
             helpers.getNextChildElementNode(helpers.getNextChildElementNode(node))
         );
         expect(resolver.lookupNamespaceURI('')).to.equal('http://www.w3.org/TR/REC-html40');
-        //Y.Assert.areSame('http://www.w3.org/TR/REC-html40', resolver.lookupNamespaceURI(''));
     });
 });
